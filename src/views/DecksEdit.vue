7d66ff9d1c8e5c4eaddc01e89e3ecc04374b43e6
Nathan Brownstein <nana8901@gmail.com>
2022-09-26 14:09:54 -0400

    added a 'delete card' function to the DecksEdit page, allow user to see the cards in the deck and delete specific cards.

--- src/views/DecksEdit.vue
+++ src/views/DecksEdit.vue
@@ -4,7 +4,8 @@
     data: function () {
       return {
         message: "Edit the deck name or description",
-        deck: {}
+        deck: {},
+        cards: []
       };
     },
     created: function () {
@@ -17,6 +18,7 @@
         axios.get("http://localhost:3000/decks/" + this.$route.params.id + ".json").then(response => {
           console.log(response.data);
           this.deck = response.data;
+          this.cards= response.data.cards
         })
       },
       updateDeck: function() {
@@ -25,6 +27,14 @@
           console.log(response.data);
           this.$router.push(`/decks/${this.deck.id}`);
         })
+      },
+      deleteCard: function (toGoCard) {
+        console.log('delete card ' + toGoCard.in_deck_id + " " + toGoCard.name)
+        axios.delete(`http://localhost:3000/cards/${toGoCard.in_deck_id}`).then(response => {
+          console.log(response)
+          window.location.reload();
+        })
+        
       }
     },
   };
@@ -36,6 +46,19 @@
     <p>Name: <input type="text" v-model="deck.name" /></p>
     <p>Description: <input type="text" v-model="deck.description" /></p>
     <button v-on:click="updateDeck()">Update</button>
+  </div>
+  <div class="deck_cards">
+    <ul>
+      <div v-for="card in this.cards" v-bind:key="card.id" >
+        <li>
+          <h4>{{card.name}} X {{card.number_in_deck}}</h4><button @click="deleteCard(card)">Delete card?</button>
+          <span class="card_image">
+            <img v-bind:src="card.image_url" />
+          </span>
+        </li>
+        <hr />
+      </div>
+    </ul>
   </div>
 </template>
 
