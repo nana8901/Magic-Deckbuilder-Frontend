e27b82150fc0820be3998d344c8aa53bff0aac6c
Nathan Brownstein <nana8901@gmail.com>
2022-09-28 12:21:21 -0400

    added a bunch of stuff, including- cards and deck_id to data, a change-card function which allows for changing the number of a card in the deck(needs to be fixed to not change all card dropdowns, but does only affect one card?), and slightly changed how cards are shown to account for basic lands allowing for more than 4 cards in a deck(possibly add in hard-coded maxes for other nonstandard cards? if adding more cards)

--- src/views/DecksEdit.vue
+++ src/views/DecksEdit.vue
@@ -5,7 +5,8 @@
       return {
         message: "Edit the deck name or description",
         deck: {},
-        cards: []
+        cards: [],
+        newCardAddParams: {}
       };
     },
     created: function () {
@@ -18,7 +19,8 @@
         axios.get("http://localhost:3000/decks/" + this.$route.params.id + ".json").then(response => {
           console.log(response.data);
           this.deck = response.data;
-          this.cards= response.data.cards
+          this.cards= response.data.cards;
+          this.newCardAddParams.deck_id = this.deck.id
         })
       },
       updateDeck: function() {
@@ -34,7 +36,9 @@
           console.log(response)
           window.location.reload();
         })
-        
+      },
+      changeCard: function (toChangeCard) {
+        console.log('change card ' + toChangeCard.in_deck_id + " " + toChangeCard.name + ' ' + this.newCardAddParams[toChangeCard.id])
       }
     },
   };
@@ -51,7 +55,18 @@
     <ul>
       <div v-for="card in this.cards" v-bind:key="card.id" >
         <li>
-          <h4>{{card.name}} X {{card.number_in_deck}}</h4><button @click="deleteCard(card)">Delete card?</button>
+          <h4>{{card.name}} <select v-model="newCardAddParams[card.id]" class="form-control sl">
+              <option v-if="card.types.includes('Basic')" v-for="num in 99" v-bind:value="num">
+                {{num}}
+              </option>
+              <option v-if="!card.types.includes('Basic')" v-for="num in [0,1,2,3,4]" v-bind:value="num">
+                {{num}}
+              </option>
+            </select>
+          </h4>
+          <button @click="this.changeCard(card)">Submit(semifunctional)</button>
+          <p />
+          <button @click="deleteCard(card)">Delete card?</button>
           <span class="card_image">
             <img v-bind:src="card.image_url" />
           </span>
