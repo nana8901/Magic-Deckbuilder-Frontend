0f93e763e719f82bfb73630e80051050e3743948
Nathan Brownstein <nana8901@gmail.com>
2022-09-29 16:41:09 -0400

    minor change to Home View, just added 'fan-made' to the greeting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/magic.jpg">
-    <h4>Hey there! Thanks for using this unofficial fan Magic the Gathering deckbuilder.</h4>
+    <h4>Hey there! Thanks for using this unofficial, fan-made Magic the Gathering deckbuilder.</h4>
     <hr />
     <h4>To get started, go to the Login section to make an account, then go to Decks and create a deck!</h4>
     <h4></h4>
